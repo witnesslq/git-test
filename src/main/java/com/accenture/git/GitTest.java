6f245695a4f7d27b786d1a66065e8ffb21a7c899
刘清 <lqwitness@163.com>
2021-05-01 20:49:33 +0800

    first commit

---
+++ src/main/java/com/accenture/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.accenture.git;
+
+/**
+ * @Description
+ * @Author lqwitness
+ * @Date 2021-05-01 20:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
