90aa26d2815718bb4352e796ce1f59ac75c89b12
刘清 <lqwitness@163.com>
2021-05-02 12:46:49 +0800

    push commit

--- src/main/java/com/accenture/git/GitTest.java
+++ src/main/java/com/accenture/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello world hot-fix");
         System.out.println("hello world master test");
         System.out.println("hello world hot-fix test");
+        System.out.println("push test");
     }
 }
