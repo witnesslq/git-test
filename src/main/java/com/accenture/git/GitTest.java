ba87c6443031536de830d2ec2645de936046292a
witnesslq <lqwitness@126.com>
2021-05-02 12:59:18 +0800

    pull test

--- src/main/java/com/accenture/git/GitTest.java
+++ src/main/java/com/accenture/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello world master test");
         System.out.println("hello world hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
